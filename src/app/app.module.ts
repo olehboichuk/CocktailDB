95b3c0328c4393e4aabf1838bebb18e3b5fd5919
Oleg <olegboychuk099@gmail.com>
2020-05-20 13:26:37 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
